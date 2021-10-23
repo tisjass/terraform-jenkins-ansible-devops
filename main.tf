5b87375bddd7a0f49f08e1155c036dcb9727253c
Kumar Haribaba <33956307+tisjass@users.noreply.github.com>
2021-10-23 12:46:18 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "app-server1" {
+
+ami = "ami-02e136e904f3da870"
+instance_type = "t2.micro"
+
+key_name = "kumar-devops"
+
+provisioner "local-exec" {
+
+   command = "echo ${aws_instance.app-server1.public_ip}   > ./inventory"
+}
+
+tags = {
+   Name = "kumar-server-devops"
+   dept = "Engineering"
+}
+}
